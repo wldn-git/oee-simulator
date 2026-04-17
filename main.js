d9737d5f07e51bc05763e4a330bf6deb0d4064f7
wldn-git <wildan.maradona@gmail.com>
2026-04-17 16:31:13 +0700

    Initial commit: OEE Simulator PRO

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1366,
+        height: 768,
+        title: "OEE Simulator PRO - SMARTEK",
+        autoHideMenuBar: true, 
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webSecurity: false // <--- INI KUNCI JAWABANNYA: Mengizinkan akses file lokal
+        }
+    });
+
+    mainWindow.maximize(); 
+    
+    // Cukup panggil nama filenya langsung, biarkan Electron yang mengatur jalurnya
+    mainWindow.loadFile('index.html'); 
+    
+    // Alat Rontgen kita matikan saja (diberi tanda komentar)
+    // mainWindow.webContents.openDevTools(); 
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
